331f6a2bdf0d56ec35b91f32dd86274839d48a85
Nate <89227631+0xNeit@users.noreply.github.com>
2025-06-20 12:19:14 +0100

    Removed masm parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use masm_parser::{MidenProgram, EmptyProgram};
-
 fn main() {
-    let pow_program = MidenProgram::parse_from_file("./masm/constants.masm").unwrap();
-    pow_program.print_masm();
+    println!("Hello, world!");
 }
